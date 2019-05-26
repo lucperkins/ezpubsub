965c7689432d0eaae392e821cc985ed4566b7e2e
lucperkins <lucperkins@gmail.com>
2019-05-26 14:49:34 +0200

    Add tests for publisher configuration

--- publisher_test.go
+++ publisher_test.go
@@ -19,4 +19,9 @@ func TestPublisher(t *testing.T) {
 	pub, err := NewPublisher(cfg)
 	is.NoError(err)
 	is.NotNil(pub)
+
+	_, err = NewPublisher(&PublisherConfig{Topic: topic})
+	is.EqualError(err, ErrNoProjectSpecified.Error())
+	_, err = NewPublisher(&PublisherConfig{Project: project})
+	is.EqualError(err, ErrNoTopicSpecified.Error())
 }
