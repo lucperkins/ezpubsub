b1ae1ade63f11660591d823e296852b88cac01ee
lucperkins <lucperkins@gmail.com>
2019-05-27 14:39:31 -0700

    Fix publisher tests

--- subscriber_test.go
+++ subscriber_test.go
@@ -2,7 +2,6 @@ package ezpubsub
 
 import (
 	"cloud.google.com/go/pubsub"
-	"context"
 	"github.com/stretchr/testify/assert"
 	"log"
 	"testing"
@@ -12,26 +11,27 @@ const (
 	subscription = "ezpub-test-sub"
 )
 
-func listen(_ context.Context, _ *pubsub.Message) {}
-
 func TestSubscribe(t *testing.T) {
 	is := assert.New(t)
 	cfg := &SubscriberConfig{
 		Project:      project,
 		Topic:        topic,
 		Subscription: subscription,
-		Listener:     listen,
 	}
 	sub, err := NewSubscriber(cfg)
 	is.NoError(err)
 	is.NotNil(sub)
+	is.NotNil(sub.listener)
+	is.NotNil(sub.errorHandler)
+	is.NotNil(sub.topic)
+	is.NotNil(sub.subscription)
 
-	_, err = NewSubscriber(&SubscriberConfig{Topic: topic, Subscription: subscription, Listener: listen})
+	_, err = NewSubscriber(&SubscriberConfig{Topic: topic, Subscription: subscription})
 	is.EqualError(err, ErrNoProjectSpecified.Error())
-	_, err = NewSubscriber(&SubscriberConfig{Project: project, Subscription: subscription, Listener: listen})
+	_, err = NewSubscriber(&SubscriberConfig{Project: project, Subscription: subscription})
 	is.EqualError(err, ErrNoTopicSpecified.Error())
-	_, err = NewSubscriber(&SubscriberConfig{Project: project, Topic: topic, Subscription: subscription})
-	is.EqualError(err, ErrNoListenerSpecified.Error())
+	_, err = NewSubscriber(&SubscriberConfig{Project: project, Topic: topic})
+	is.EqualError(err, ErrNoSubscriptionSpecified.Error())
 	_, err = NewSubscriber(&SubscriberConfig{})
 	is.EqualError(err, ErrNoProjectSpecified.Error())
 }
@@ -41,8 +41,13 @@ func ExampleSubscriber() {
 		Project:      "...",
 		Topic:        "...",
 		Subscription: "...",
-		Listener: func(_ context.Context, msg *pubsub.Message) {
+		Listener: func(msg *pubsub.Message) {
 			log.Printf("Message received (id: %s, payload: %s)", msg.Data, string(msg.Data))
+
+			msg.Ack()
+		},
+		ErrorHandler: func(err error) {
+			log.Printf("Publisher error: %v", err)
 		},
 	}
 
