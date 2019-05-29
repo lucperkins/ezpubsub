41e92702bc27b70f4f4a40ed7dbd1551fd2163d0
lucperkins <lucperkins@gmail.com>
2019-05-29 15:42:11 -0700

    Add SimpleAckListener

---
+++ listener.go
@@ -0,0 +1,11 @@
+package ezpubsub
+
+import "cloud.google.com/go/pubsub"
+
+var (
+	// A subscriber listener functions that does nothing but ack each message. Useful in such situations where you need
+	// to "wind through" outstanding messages without processing them.
+	SimpleAckListener = func(msg *pubsub.Message) {
+		msg.Ack()
+	}
+)
