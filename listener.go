b060b6fca611492fe3d663182ab3d2c5d15b5c7d
lucperkins <lucperkins@gmail.com>
2019-05-29 19:05:41 -0700

    Make SimpleAckListener a function instead of a variable

--- listener.go
+++ listener.go
@@ -2,10 +2,8 @@ package ezpubsub
 
 import "cloud.google.com/go/pubsub"
 
-var (
-	// A subscriber listener function that does nothing but ack each message. Useful in such situations where you need
-	// to "wind through" outstanding messages without processing them.
-	SimpleAckListener = func(msg *pubsub.Message) {
-		msg.Ack()
-	}
-)
+// A subscriber listener function that does nothing but ack each message. Useful in such situations where you need
+// to "wind through" outstanding messages without processing them.
+func SimpleAckListener(msg *pubsub.Message) {
+	msg.Ack()
+}
